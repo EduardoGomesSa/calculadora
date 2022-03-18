de3e50956a6b3768fdd9747a40a1dea521aad732
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2022-03-18 15:35:47 -0300

    Calculadora em JS versão 1

---
+++ aquivos - calculadora/_js/code.js
@@ -0,0 +1,8 @@
+function calculadora(n){
+    mostrar(n)
+}
+function mostrar(r){
+    var tela = r;
+    var visualizar = document.getElementById('resultado')
+    visualizar.innerHTML = 'aaaa';
+}
